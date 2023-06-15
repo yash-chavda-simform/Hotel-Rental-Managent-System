2a6fb3730351b7143be19507b5d039a5392747c3
yash <yash.c@simformsolutions.com>
2023-06-15 14:31:59 +0530

    TE7:T1003:AccountSetting:Feat:account setting is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  attr_accessor :access_token
   validates :first_name, :last_name, presence: true
+  validates :phone_no, :driving_license_no, uniqueness: true, on: :update
   validates :phone_no, :gender, :driving_license_no, :date_of_birth, presence: true, on: :update
   validates :phone_no, length: { is:10 }, on: :update
-  validates :phone_no, numericality: true
-  validates :driving_license_no, format: { with: /\A[A-Za-z0-9]{6,12}\z/, message: "Enter valid Driving License" }
-  validates :date_of_birth, comparison: { less_than: Date.today, message: "date can not be today"}
-  # validates :gender, inclusion: { in: %w(Male Female Others), message: "should be from this given category" }, on: :update
+  validates :phone_no, numericality: true, on: :update
+  validates :driving_license_no, format: { with: /\A[A-Za-z0-9]{6,12}\z/, message: "Enter valid Driving License" }, on: :update
+  validates :date_of_birth, comparison: { less_than: Date.today, message: "date can not be today"}, on: :update
   after_create :assign_default_role
 
   rolify
